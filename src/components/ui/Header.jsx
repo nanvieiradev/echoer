46163737a993546abda0b1f7248120c726ed5917
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-11 07:57:10 -0300

    feat: add header

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,10 +1,12 @@
-import Navbar from "./Navbar"
+import NavbarMenu from "./NavbarMenu"
+import NavbarSocial from "./NavbarSocial"
 
 export default function Header() {
   return (
-    <header>
-      <h1 className="mb-6 font-bold text-3xl">Meu Blog</h1>
-      <Navbar />
+    <header className="flex justify-between items-center py-9">
+      <h1 className="mb-6 font-bold text-2xl">Echoer</h1>
+      <NavbarMenu />
+      <NavbarSocial />
     </header>
   )
 }
