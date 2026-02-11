2a594b5c6dab6337dfec50392c62252313863132
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-11 08:01:33 -0300

    feat: add páginas sobre e contato

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import Head from "next/head"
+import DefaultLayout from "../layouts/DefaultLayout"
+
+export default function Contact() {
+  return (
+    <>
+      <Head>
+        <title>Echoer</title>
+      </Head>
+      <DefaultLayout>{/* lista de posts */}</DefaultLayout>
+    </>
+  )
+}
