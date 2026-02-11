00042d18e7f52d3d3490cf793f794e7aabe5a25d
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-11 09:56:38 -0300

    refactor: update atributos do input

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -5,11 +5,13 @@ const InputField = styled.input`
   color: ${({ theme }) => theme.colors.muted};
 `
 
-export default function Input({ type, placeholder }) {
+export default function Input({ type, name, placeholder, method = "post" }) {
   return (
     <InputField
       type={type}
+      name={name}
       placeholder={placeholder}
+      method={method}
       className="px-10 py-5 border-2 border-none font-light text-lg"
     />
   )
