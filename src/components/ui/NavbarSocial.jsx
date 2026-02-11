cdcc9b979baf3144044cd42573ca1053a4fc79f5
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-11 07:57:56 -0300

    feat: add navbar social

---
+++ src/components/ui/NavbarSocial.jsx
@@ -0,0 +1,9 @@
+export default function NavbarSocial() {
+  return (
+    <nav className="flex space-x-6">
+      <button className="bg-black rounded-sm w-6 h-6 cursor-pointer"></button>
+      <button className="bg-black rounded-sm w-6 h-6 cursor-pointer"></button>
+      <button className="bg-black rounded-sm w-6 h-6 cursor-pointer"></button>
+    </nav>
+  )
+}
