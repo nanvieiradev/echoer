f01d9d2db3a2a356fc11b676458f01e1ddcf8886
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-11 09:56:15 -0300

    style: update cores dos ícones sociais

--- src/components/ui/NavbarSocial.jsx
+++ src/components/ui/NavbarSocial.jsx
@@ -1,12 +1,22 @@
-import { FaLinkedin } from "react-icons/fa"
-import { FaGithub } from "react-icons/fa"
+import { IconContext } from "react-icons"
+import { FaLinkedin, FaGithub } from "react-icons/fa"
+import { useTheme } from "styled-components"
 
 export default function NavbarSocial() {
+  const theme = useTheme()
+
   return (
-    <nav className="flex space-x-6">
-      <FaLinkedin className="w-5 h-5" />
-      <FaGithub className="w-5 h-5" />
-      <button className="bg-black rounded-sm w-5 h-5 cursor-pointer"></button>
-    </nav>
+    <IconContext.Provider
+      value={{
+        color: theme.colors.foreground,
+        className: "global-class-name",
+      }}
+    >
+      <nav className="flex space-x-6">
+        <FaLinkedin className="w-5 h-5" />
+        <FaGithub className="w-5 h-5" />
+        <button className="bg-black rounded-sm w-5 h-5 cursor-pointer"></button>
+      </nav>
+    </IconContext.Provider>
   )
 }
