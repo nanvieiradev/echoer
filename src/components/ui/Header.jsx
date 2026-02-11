3357d21f8a6a0de0479032827abd03f7fc9b1cfb
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-11 09:40:37 -0300

    feat: add direcionamento para home através da logo

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,10 +1,13 @@
+import Link from "next/link"
 import NavbarMenu from "./NavbarMenu"
 import NavbarSocial from "./NavbarSocial"
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center py-9">
-      <h1 className="mb-6 font-bold text-2xl">Echoer</h1>
+      <Link href="/" className="mb-6 font-bold text-3xl">
+        Echoer
+      </Link>
       <NavbarMenu />
       <NavbarSocial />
     </header>
