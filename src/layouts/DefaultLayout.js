7b5147931f852a6e78001f44c5eb97436c65479d
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-11 09:43:54 -0300

    refactor: update nome da tag main

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -2,15 +2,11 @@ import styled from "styled-components"
 import Header from "../components/ui/Header"
 import Footer from "../components/ui/Footer"
 
-const Main = styled.main.attrs({
-  className: "flex-1 max-w-4xl px-4 py-8",
-})``
-
 export default function DefaultLayout({ children }) {
   return (
     <>
       <Header />
-      <Main>{children}</Main>
+      <main>{children}</main>
       <Footer />
     </>
   )
