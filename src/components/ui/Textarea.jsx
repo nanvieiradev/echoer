2a2e8541766de1a166484e9da4ed580947a8054e
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-12 13:49:16 -0300

    feature: add textarea

---
+++ src/components/ui/Textarea.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+const TextAreaWrapper = styled.textarea`
+  background-color: ${({ theme }) => theme.colors.input};
+  color: ${({ theme }) => theme.colors.muted};
+  border: 1px solid ${({ theme }) => theme.colors.ring};
+  padding: 0.75rem; /* p-3 */
+  font-size: 1rem;
+`
+
+export default function Textarea() {
+  return (
+    <TextAreaWrapper
+      cols="30"
+      rows="3"
+      className="focus:outline-2 focus:outline-offset-2 min-h-24.5 max-h-64 transition-colors"
+    />
+  )
+}
