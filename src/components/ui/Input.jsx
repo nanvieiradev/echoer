c92ab11e8a4d91c5c2b5c5b641d580740ac23b34
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-12 13:50:22 -0300

    style: update variantes do input

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,18 +1,43 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
-const InputField = styled.input`
-  background-color: ${({ theme }) => theme.colors.background};
-  color: ${({ theme }) => theme.colors.muted};
+const variants = {
+  primary: css`
+    background-color: ${({ theme }) => theme.colors.input};
+    color: ${({ theme }) => theme.colors.muted};
+    border: 1px solid ${({ theme }) => theme.colors.foreground};
+    padding: 1.25rem 2rem; /* px-10 py-5 */
+    font-weight: 300; /* font-light */
+    outline-color: ${({ theme }) => theme.colors.primary};
+  `,
+  outlined: css`
+    background-color: ${({ theme }) => theme.colors.input};
+    color: ${({ theme }) => theme.colors.muted};
+    border: 1px solid ${({ theme }) => theme.colors.ring};
+    padding: 0.75rem; /* p-3 */
+    font-size: 1rem;
+  `,
+}
+
+const InputField = styled.input.attrs({
+  className: "transition-colors focus:outline-2 focus:outline-offset-2",
+})`
+  ${({ variant }) => variants[variant] || variants.primary}
 `
 
-export default function Input({ type, name, placeholder, method = "post" }) {
+export default function Input({
+  type = "text",
+  name,
+  placeholder,
+  method = "post",
+  variant = "primary",
+}) {
   return (
     <InputField
       type={type}
       name={name}
       placeholder={placeholder}
       method={method}
-      className="px-10 py-5 border-2 border-none font-light text-lg"
+      variant={variant}
     />
   )
 }
