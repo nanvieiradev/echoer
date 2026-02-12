5061a1f7497b00f90bc4d7965765189b9b27cc86
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-12 13:52:46 -0300

    style: update fonte -> poppinss

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,13 +1,23 @@
 import Link from "next/link"
 import NavbarSocial from "./NavbarSocial"
+import { Poppins } from "next/font/google"
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+})
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center gap-6 py-24">
+    <footer
+      className={`flex flex-col items-center gap-6 py-24 ${poppins.className}`}
+    >
       <NavbarSocial />
       <p>
         Feito por{" "}
-        <Link href="/" className="underline underline-offset-2">@nanvieiradev</Link>
+        <Link href="/" className="underline underline-offset-2">
+          @nanvieiradev
+        </Link>
       </p>
     </footer>
   )
