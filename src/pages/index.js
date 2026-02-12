12b17871ff1d156f29077bafd53ced315872f552
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-12 18:39:04 -0300

    feat: add lista de posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
 import Head from "next/head"
 import DefaultLayout from "../layouts/DefaultLayout"
 import Newsletter from "@/components/Newsletter"
+import fs from "fs"
+import path from "path"
+import matter from "gray-matter"
+import PostList from "@/components/PostList"
 
 export default function Home({ posts }) {
   return (
@@ -9,9 +13,23 @@ export default function Home({ posts }) {
         <title>Echoer</title>
       </Head>
       <DefaultLayout>
-        {/* lista de posts */}
+        <PostList posts={posts} />
         <Newsletter />
       </DefaultLayout>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("src/content/posts"))
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".mdx", "")
+    const markdownWithMeta = fs.readFileSync(
+      path.join("src/content/posts", filename),
+      "utf-8",
+    )
+    const { data } = matter(markdownWithMeta)
+    return { slug, ...data }
+  })
+  return { props: { posts } }
 }
