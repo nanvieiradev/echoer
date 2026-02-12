a3b0ee66f57a919e73257a39976de2f111c376d1
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-12 13:48:19 -0300

    chore: add implementação do placehold.co

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,18 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    dangerouslyAllowSVG: true, // Necessário se usar placeholders em SVG
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", // Recomendado com SVG
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 }
 
 export default nextConfig;
