7740b6a8ee4fcec567219093ec779726c2c4f285
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-12 13:51:39 -0300

    refactor: update redirecionaentos da navbar social

--- src/components/ui/NavbarSocial.jsx
+++ src/components/ui/NavbarSocial.jsx
@@ -1,8 +1,9 @@
+import Link from "next/link"
 import { IconContext } from "react-icons"
-import { FaLinkedin, FaGithub } from "react-icons/fa"
+import { FaLinkedinIn, FaGithub } from "react-icons/fa"
 import { useTheme } from "styled-components"
 
-export default function NavbarSocial() {
+export default function NavbarSocial({ className }) {
   const theme = useTheme()
 
   return (
@@ -12,9 +13,13 @@ export default function NavbarSocial() {
         className: "global-class-name",
       }}
     >
-      <nav className="flex space-x-6">
-        <FaLinkedin className="w-5 h-5" />
-        <FaGithub className="w-5 h-5" />
+      <nav className={`${className} flex space-x-6`}>
+        <Link href="/">
+          <FaLinkedinIn className="w-5 h-5" />
+        </Link>
+        <Link href="/">
+          <FaGithub className="w-5 h-5" />
+        </Link>
         <button className="bg-black rounded-sm w-5 h-5 cursor-pointer"></button>
       </nav>
     </IconContext.Provider>
