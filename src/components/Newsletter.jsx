63b6e7d00d05f57c9fdb9e99723bc57e30a3ffb6
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-12 13:53:45 -0300

    style: update tamanho de texto e espaçamento da newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -18,19 +18,15 @@ const Text = styled.p`
 export default function Newsletter() {
   return (
     <NewsletterSection className="flex flex-col items-center gap-12 py-32 text-center">
-      <Title className="mb-6 font-bold text-4xl">
+      <Title className="mb-9 font-medium text-4xl">
         Assine nossa newsletter.
       </Title>
-      <Text className="max-w-lg font-light text-lg">
+      <Text className="max-w-xl font-light text-lg">
         Cadastre seu endereço de e-mail para receber novidades e atualizações.
       </Text>
-      <Form action="/" className="space-x-3">
-        <Input
-          type="email"
-          name="email"
-          placeholder="Endereço de e-mail"
-        />
-        <Button type="submit" variant="primary">
+      <Form action="/" className="flex justify-center gap-3">
+        <Input type="email" name="email" placeholder="Endereço de e-mail" />
+        <Button type="submit" onClick={(e) => e.preventDefault()}>
           Cadastre-se
         </Button>
       </Form>
