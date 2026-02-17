ef173bd6be0cfb474eeadcb55baeaf1190be01f2
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-16 22:48:43 -0300

    style: update responsividade do header e menus

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -3,15 +3,21 @@ import MobileMenu from "./MobileMenu"
 import Link from "next/link"
 import NavbarMenu from "./NavbarMenu"
 import NavbarSocial from "./NavbarSocial"
+import styled from "styled-components"
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 })
 
+const HeaderWrapper = styled.header`
+  background-color: ${({ theme }) => theme.colors.background};
+  color: ${({ theme }) => theme.colors.foreground};
+`
+
 export default function Header() {
   return (
-    <header className={`${poppins.className} py-6 sm:py-9`}>
+    <HeaderWrapper className={`${poppins.className} py-6 sm:py-9 z-0`}>
       <nav className="hidden sm:flex justify-between items-center">
         <Link href="/" className="font-medium text-3xl">
           Echoer
@@ -20,6 +26,6 @@ export default function Header() {
         <NavbarSocial />
       </nav>
       <MobileMenu />
-    </header>
+    </HeaderWrapper>
   )
 }
