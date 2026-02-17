435f3bfcceeba0da11bb530e2d3309179895ac7e
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-17 18:18:40 -0300

    style: update tipografia geral

--- src/components/ui/NavbarMenu.jsx
+++ src/components/ui/NavbarMenu.jsx
@@ -2,7 +2,7 @@ import PageLink from "./PageLink"
 
 export default function NavbarMenu() {
   return (
-    <nav className="space-x-6">
+    <nav className="space-x-6 font-light">
       <PageLink href="/">Blog</PageLink>
       <PageLink href="/about">Sobre</PageLink>
       <PageLink href="/contact">Contato</PageLink>
--- src/components/ui/Post.jsx
+++ src/components/ui/Post.jsx
@@ -4,20 +4,21 @@ import Link from "next/link"
 export default function Post({ post }) {
   return (
     <div className="flex flex-col items-center gap-6 text-center post">
-      <Link href={`/posts/${post.slug}`}>
+      <Link href={`/posts/${post.slug}`} className="w-full">
         <Image
           src={post.image}
           alt={post.title}
-          width={post.sizes[0]}
-          height={post.sizes[1]}
-          className="border border-gray-300"
+          width={100}
+          height={100}
+          layout="responsive"
+          className="border border-gray-300 w-full cover"
         />
       </Link>
-      <span className="-mt-3 text-sm">{post.date}</span>
+      <span className="-mt-2 text-sm">{post.date}</span>
       <Link href={`/posts/${post.slug}`}>
-        <h2 className="font-medium text-2xl">{post.title}</h2>
+        <h2 className="font-medium text-2xl xl:text-3xl">{post.title}</h2>
       </Link>
-      <p className="max-w-md font-light text-sm">{post.summary}</p>
+      <p className="max-w-lg font-light text-sm md:text-base leading-6">{post.summary}</p>
       <Link
         href={`/posts/${post.slug}`}
         className="font-light text-sm underline underline-offset-4"
