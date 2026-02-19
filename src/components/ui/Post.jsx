916f45ef828e0e645f63b9d10e5d3fec925177e4
Renan Vieira <112444278+nanvieiradev@users.noreply.github.com>
2026-02-19 12:12:18 -0300

    style: update formato da data no post

--- src/components/ui/Post.jsx
+++ src/components/ui/Post.jsx
@@ -1,7 +1,16 @@
 import Image from "next/image"
 import Link from "next/link"
+import styled from "styled-components"
 
 export default function Post({ post }) {
+  const PostDate = styled.span`
+    color: ${({ theme }) => theme.colors.muted};
+  `
+
+  const formattedDate = new Date(
+    post.date.replace(/-/g, "/"),
+  ).toLocaleDateString("pt-BR")
+
   return (
     <div className="flex flex-col items-center gap-6 text-center post">
       <Link href={`/posts/${post.slug}`} className="w-full">
@@ -12,13 +21,17 @@ export default function Post({ post }) {
           height={100}
           layout="responsive"
           className="border border-gray-300 w-full cover"
+          unoptimized
+          loading="eager"
         />
       </Link>
-      <span className="-mt-2 text-sm">{post.date}</span>
+      <PostDate className="-mt-2 text-sm">{formattedDate}</PostDate>
       <Link href={`/posts/${post.slug}`}>
         <h2 className="font-medium text-2xl xl:text-3xl">{post.title}</h2>
       </Link>
-      <p className="max-w-lg font-light text-sm md:text-base leading-6">{post.summary}</p>
+      <p className="max-w-lg font-light text-sm md:text-base leading-6">
+        {post.summary}
+      </p>
       <Link
         href={`/posts/${post.slug}`}
         className="font-light text-sm underline underline-offset-4"
